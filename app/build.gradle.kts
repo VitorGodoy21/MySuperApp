bb78484806d8b53f8cd67593f87f7e4f3d19153b
vitorfgodoy123 <vitorfgodoy@gmail.com>
2026-01-29 23:59:29 -0300

    Add Firebase, Hilt, and Coroutines dependencies and configure product flavors
    - Add Firebase (Auth, Firestore, Analytics), Hilt, Coroutines, and DataStore dependencies
    - Configure `dev` and `prd` product flavors with custom `applicationIdSuffix` and `resValue`
    - Update Android Gradle Plugin, Compose BOM, and other dependency versions
    - Enable `buildConfig` and set `jvmTarget` to 11
    - Add Google Services and Hilt plugins to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+    kotlin("kapt")
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.vfdeginformatica.mysuperapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.vfdeginformatica.mysuperapp"
@@ -19,6 +21,24 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    flavorDimensions += "env"
+
+    productFlavors {
+
+        create("dev") {
+            dimension = "env"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+            buildConfigField("boolean", "IS_DEBUGGABLE", "true")
+            resValue("string", "app_name", "DEV-MySuperApp")
+        }
+
+        create("prd") {
+            dimension = "env"
+            buildConfigField("boolean", "IS_DEBUGGABLE", "false")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,14 +46,28 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    androidComponents {
+        beforeVariants(selector().withBuildType("debug")) {
+            it.enable = false
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -42,15 +76,40 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.ui.tooling)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.ui.auth)
+    implementation(libs.firebase.firestore)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    //Coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    implementation(libs.androidx.lifecycle.runtime.ktx.v284)
+
+    //Icons Default
+    implementation(libs.androidx.material.icons.extended)
+
+    //Data Store Preferences
+    implementation(libs.androidx.datastore.preferences)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.service) apply false
+}
+
+buildscript{
+    repositories{
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        // Dependency added to ensure compatibility with recent versions.
+        classpath(libs.javapoet)
+    }
 }
