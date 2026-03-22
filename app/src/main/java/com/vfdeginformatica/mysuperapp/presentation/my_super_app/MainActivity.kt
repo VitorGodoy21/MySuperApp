0cc426b532d895f7fa34fb10dd341c04a172024a
vitorfgodoy123 <vitorfgodoy@gmail.com>
2026-03-22 19:54:19 -0300

    Update MainActivity to extend AppCompatActivity and use AppCompat theme
    - Change `MainActivity` inheritance from `ComponentActivity` to `AppCompatActivity`
    - Update `Theme.MySuperApp` parent style to `Theme.AppCompat.Light.NoActionBar`

--- app/src/main/java/com/vfdeginformatica/mysuperapp/presentation/my_super_app/MainActivity.kt
+++ app/src/main/java/com/vfdeginformatica/mysuperapp/presentation/my_super_app/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.vfdeginformatica.mysuperapp.presentation.my_super_app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
