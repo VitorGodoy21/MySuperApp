9e6add752b9fb882ea46ab2d8056bea57efc584b
vitorfgodoy123 <vitorfgodoy@gmail.com>
2026-03-25 19:37:57 -0300

    Refactor QrCodeGenerator to use Android KTX extension functions
    - Replace `Bitmap.createBitmap` with `createBitmap` KTX function
    - Use operator overloading for setting bitmap pixels with `bitmap[x, y]` instead of `setPixel`

--- app/src/main/java/com/vfdeginformatica/mysuperapp/presentation/util/QrCodeGenerator.kt
+++ app/src/main/java/com/vfdeginformatica/mysuperapp/presentation/util/QrCodeGenerator.kt
@@ -1,6 +1,8 @@
 package com.vfdeginformatica.mysuperapp.presentation.util
 
 import android.graphics.Bitmap
+import androidx.core.graphics.createBitmap
+import androidx.core.graphics.set
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.common.BitMatrix
@@ -14,15 +16,11 @@ object QrCodeGenerator {
                 size,
                 size
             )
-            val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565)
+            val bitmap = createBitmap(size, size, Bitmap.Config.RGB_565)
 
             for (x in 0 until size) {
                 for (y in 0 until size) {
-                    bitmap.setPixel(
-                        x,
-                        y,
-                        if (bitMatrix[x, y]) 0xFF000000.toInt() else 0xFFFFFFFF.toInt()
-                    )
+                    bitmap[x, y] = if (bitMatrix[x, y]) 0xFF000000.toInt() else 0xFFFFFFFF.toInt()
                 }
             }
 
